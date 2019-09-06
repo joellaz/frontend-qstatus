686cb1d1c3d72801a8163b8808bf2ab86503ffbb
joellaz <joelle_zw@hotmail.com>
2019-09-06 16:56:48 +0200

    eerste commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Gist
+https://gist.github.com/joellaz/7b8d6771e6bdcb960f38b61d7de67469
+
+### Install
+
+```sh
+npm install
+```
---
+++ index.js
@@ -0,0 +1,26 @@
+var animalKingdom = {
+    mammals: [
+        {
+            species:  'lion',
+            mane: true,
+            limbs: 4,
+            tail: true
+        },
+        {
+            species:  'human',
+            mane: false,
+            limbs: 4,
+            tail: false
+        } 
+    ]
+};
+
+var allMammals = animalKingdom.mammals;
+var onlyLion = allMammals.filter(function(mammals){
+    if(mammals.species === 'lion'){
+        return true;
+    }
+    return false;
+});
+
+console.log(onlyLion);
