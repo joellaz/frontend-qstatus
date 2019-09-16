a94529880833842517af0ca3d25a0d1285adb18c
Joren Broekema <joren.broekema@ing.com>
2019-09-16 15:04:05 +0200

    chore: improve qstates render

--- components/yc-qstates/yc-qstates.js
+++ components/yc-qstates/yc-qstates.js
@@ -4,8 +4,8 @@ import 'yc-battery/yc-battery.js';
 class YCQstates extends LitElement {
   static get properties() {
     return {
-      qData: {
-        type: Object,
+      dataObj: {
+        type: Array,
         hasChanged(newVal, oldVal) {
           if (JSON.stringify(newVal) !== JSON.stringify(oldVal)) {
             return true;
@@ -17,37 +17,89 @@ class YCQstates extends LitElement {
   }
 
   static get styles() {
-    return css``;
+    return css`
+      :host {
+        display: flex;
+        flex-direction: column;
+      }
+
+      .row {
+        display: flex;
+      }
+
+      .col {
+        padding: 0 20px;
+      }
+
+      .q-title {
+        text-align: center;
+      }
+    `;
   }
 
-  connectedCallback() {
+  async connectedCallback() {
     super.connectedCallback();
-    this.fetchQStates();
+    await this.fetchQStates();
+
+    // Create an obj from database result that is easier to work with in template :)
+    this.dataObj = [
+      [
+        {
+          name: 'IQ',
+          balance: this.qData.iq_state
+        },
+        {
+          name: 'EQ',
+          balance: this.qData.eq_state
+        }
+      ],
+      [
+        {
+          name: 'FQ',
+          balance: this.qData.fq_state
+        },
+        {
+          name: 'SQ',
+          balance: this.qData.sq_state
+        }
+      ]
+    ];
   }
 
   async fetchQStates() {
     const response = await fetch('../../assets/mock-data/qstates.json');
+    await new Promise(resolve => {
+      setTimeout(resolve, 2000);
+    });
     this.qData = await response.json();
-    console.log('just fetched new qData');
-
-    // Necessary to manually start a re-render, because now we have the fillAmounts from the API.
-    // this.requestUpdate();
   }
 
   render() {
     return html`
-      <yc-battery
-        .fillAmount=${this.qData ? this.qData.iq_state * 100 : 0}
-      ></yc-battery>
-      <yc-battery
-        .fillAmount=${this.qData ? this.qData.eq_state * 100 : 0}
-      ></yc-battery>
-      <yc-battery
-        .fillAmount=${this.qData ? this.qData.fq_state * 100 : 0}
-      ></yc-battery>
-      <yc-battery
-        .fillAmount=${this.qData ? this.qData.sq_state * 100 : 0}
-      ></yc-battery>
+      <div class="row">
+        ${this.dataObj
+          ? this.dataObj[0].map(
+              obj => html`
+                <div class="col">
+                  <p class="q-title">${obj.name}</p>
+                  <yc-battery .fillAmount=${obj.balance * 100}></yc-battery>
+                </div>
+              `
+            )
+          : 'Loading...'}
+      </div>
+      <div class="row">
+        ${this.dataObj
+          ? this.dataObj[1].map(
+              obj => html`
+                <div class="col">
+                  <p class="q-title">${obj.name}</p>
+                  <yc-battery .fillAmount=${obj.balance * 100}></yc-battery>
+                </div>
+              `
+            )
+          : 'Loading...'}
+      </div>
     `;
   }
 }
