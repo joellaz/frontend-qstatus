f6b45c05ddb2e887e08649683d5cbe7960d826c4
michiel <pauw.michiel@gmail.com>
2019-09-16 16:38:58 +0200

    Added answer-element

---
+++ answer-element.js
@@ -0,0 +1,118 @@
+import { LitElement, html, css } from 'lit-element';
+
+class AnswerElement extends LitElement {
+  static get properties() {
+    return {
+      questionId: {
+        type: Number
+      }
+    };
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+    await this.getQuestionList();
+  }
+
+  async getQuestionList() {
+    const response = await fetch('http://localhost:8082/questionlist/1');
+    if (response.status === 200) {
+      const dataJson = await response.json();
+      this.questionList = dataJson.questionList;
+      this.questionId = 0;
+    }
+  }
+
+  sendOpenAnswer(e) {
+    const inputValue = e.target.parentNode.querySelector('input').value;
+
+    let sendableAnswer = {};
+    sendableAnswer.title = 'openquestion';
+    sendableAnswer.text = inputValue;
+
+    const request = fetch('http://localhost:8082/openanswer', {
+      method: 'POST',
+      body: JSON.stringify(sendableAnswer),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    this.questionId++;
+  }
+
+  render() {
+    return html`
+      <div style="margin: 0 auto;">
+        <form>
+          <h2 style="margin-top:50px">Vraag</h2>
+          <h3>
+            ${this.questionList
+              ? this.questionList[this.questionId].text
+              : 'Loading...'}
+          </h3>
+          <!--  <button type="button" onclick="loadDoc()">Laat de vraag zien en <br>typ je antwoord hieronder.</button>  -->
+          <p>Typ je antwoord in het antwoordveld.</p>
+
+          ${this.questionList[this.questionId].questionType === 'OPEN'
+            ? html`
+                <input
+                  @keypress=${e => {
+                    if (e.key === 'Enter') {
+                      this.sendOpenAnswer(e);
+                      console.log('pressed enter on input');
+                    }
+                  }}
+                  type="text"
+                  name="newAnswer"
+                  style="font-size:12pt;height:200px;width:50%;"
+                />
+              `
+            : html`
+                <!-- Template for closed questions with radios -->
+              `}
+
+          <button
+            type="button"
+            class=""
+            style="margin-top:30px"
+            @click=${e => this.sendOpenAnswer(e)}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    `;
+  }
+
+  createRenderRoot() {
+    return this;
+  }
+}
+
+let questionList;
+
+function showQuestion(question) {}
+
+// Wat gebeurt hier?
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var vraag = this.responseText;
+      var string = JSON.parse(vraag);
+      // welke variable meot hij hieraanroepeN?
+      document.getElementById('myData').innerHTML = string.text;
+    }
+  };
+
+  // welke variabelen
+  var identify = xhttp.open(
+    //id ergens vandaag???
+    'GET',
+    'http://localhost:8082/openquestion/{' + identify + '}',
+    true
+  );
+  xhttp.send();
+}
+
+customElements.define('answer-element', AnswerElement);
